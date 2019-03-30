68884b7be9929d4b7621c9c155bf47768e3a85de
SlamDewey <mr.jaredmassa@gmail.com>
2019-03-29 21:19:24 -0400

    setup commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Home from './pages/Home/Home.js';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class App extends Component {
+    render() {
+        return(
+            <Switch>
+                <Route exact path={'/'} render={() => <Home />} />
+            </Switch>
+        );
+    }
+}
+
+ReactDOM.render(
+<Router>
+    <App />
+</Router>,
+root);
+
 serviceWorker.unregister();
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class Home extends Component {
+
+    fetch_street_map() {
+
+    }
+
+    render() {
+        this.fetch_street_map();
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
