deb45a9d2f54c9a977d6fa4865f98bff197cd266
SlamDewey <mr.jaredmassa@gmail.com>
2019-03-29 20:34:50 -0400

    Updates

---
+++ precache-manifest.68ecb276a595f4ce00787883432ce405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a74ab510b637f2c9b0e6",
+    "url": "/static/js/main.da4c011d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a74ab510b637f2c9b0e6",
+    "url": "/static/css/main.57aed8b0.chunk.css"
+  },
+  {
+    "revision": "cc65e7be2018869e454c4145eb358efc",
+    "url": "/index.html"
+  }
+];
