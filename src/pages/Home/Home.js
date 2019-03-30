45b8000b30583b3f63b3af1c43922ce625dbe62a
SlamDewey <mr.jaredmassa@gmail.com>
2019-03-30 11:20:19 -0400

    added map data .osm

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,14 @@ import React, {Component} from 'react';
 class Home extends Component {
 
     fetch_street_map() {
-
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(response.text);
+            }
+        };
+        xhttp.open("GET", "home.css", true);
+        xhttp.send();
     }
 
     render() {
