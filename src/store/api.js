d52aef02b49ee153c35cb830bd8bc143ce2bb415
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2019-03-30 15:08:20 -0400

    api usage example

--- src/store/api.js
+++ src/store/api.js
@@ -1,3 +1,13 @@
-export const get_map = (payload) => {
-    return {map: "hello"}
+const request = require('request');
+
+function sometn(body) {
+    console.log(body)
 }
+
+export const get_map = (payload, callback) => {
+    request.post('http://localhost:8081/getMapRoutes', {form:{key:'value'}}, (error, response, body) => {
+        callback(body)
+    })
+}
+
+// get_map()
