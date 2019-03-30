05d402fa4531c9d51964d6d707f64a2bcc78d36a
SlamDewey <mr.jaredmassa@gmail.com>
2019-03-29 20:35:56 -0400

    Updates

---
+++ precache-manifest.80c406da0bff596421bd2f43aad41174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/temple-crime-router/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f9c2f1acc11dbece0e1",
+    "url": "/temple-crime-router/static/js/runtime~main.cb71fe57.js"
+  },
+  {
+    "revision": "503267962d36ec43e7e3",
+    "url": "/temple-crime-router/static/js/main.eb3a3eae.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/temple-crime-router/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "503267962d36ec43e7e3",
+    "url": "/temple-crime-router/static/css/main.57aed8b0.chunk.css"
+  },
+  {
+    "revision": "2f599a85a624bfd82a67b91ec55733e8",
+    "url": "/temple-crime-router/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68ecb276a595f4ce00787883432ce405.js"
+  "/temple-crime-router/precache-manifest.80c406da0bff596421bd2f43aad41174.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/temple-crime-router/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
