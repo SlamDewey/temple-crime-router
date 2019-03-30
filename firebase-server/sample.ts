6f349266ae1d67ae7e6d83822c952364954e754f
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2019-03-30 14:54:38 -0400

    api server works

--- firebase-server/sample.ts
+++ firebase-server/sample.ts
@@ -13,19 +13,8 @@ var db = admin.database();
 
 function pull_data() {
 	var ref = db.ref("users/");
-	// var dir = path.join('data/', version)
-	// fs.mkdirSync(dir, {recursive: true}, (err) => {
-	// 	if(err) throw err;
-	// });
-	// fs.writeFileSync("data/readme.txt", "run 'node pull_data' to populate this folder", function(err) {
-	// 	if(err) throw err;
-	// });
 	ref.once("value", function(data) {
 		data.forEach(function(data) {
-			// fs.writeFile(path.join(dir,data.key + ".json"), JSON.stringify(data.val()), function(err) {
-			// 	if(err) throw err
-			// 	console.log("saved file!")
-            // })
             console.log(data.val())
 		});
 		console.log("done!")
